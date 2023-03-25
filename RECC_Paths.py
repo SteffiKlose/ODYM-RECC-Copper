7a323878b4770f6b3ce3d7eb99a21fcec8e1b310
SteffiKlose <stefanie.klose@posteo.de>
2023-03-25 13:39:48 +0100

    Add files via upload

---
+++ RECC_Paths.py
@@ -0,0 +1,16 @@
+'''
+This is the ODYm-RECC Copper model path file. RECC will use the paths specified here to search for the necessary data and modules.
+'''
+
+### Start of path file ###
+### These paths are on your own machine ###
+
+current = 'Enter here the current path where you saved the ODYM-RECC Copper main code'
+
+odym_path = 'Enter here the path where you saved the other ODYM-RECC Copper modules'
+data_path = 'Enter here the path where you saved the ODYM-RECC Copper data from ZENODO'
+results_path = 'Path where the results shall be saved'
+
+
+### End of path file ###
+
